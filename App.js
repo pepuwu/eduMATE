a6bb94b8010be90f50e8fb12b45fd97306b3a02d
gashino <agaggino@uade.edu.ar>
2024-06-18 01:21:55 -0300

    fixcommit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const Stack = createNativeStackNavigator();
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import MIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import InicioAlumnoScreen from "./screens/InicioAlumno";
@@ -14,9 +15,23 @@ import EscaneoScreen from "./screens/Escaneo";
 import NotificarScreen from "./screens/Notificar";
 import PerfilScreen from "./screens/Perfil";
 import LoginScreen from "./screens/Login";
-import { Image } from "expo-image";
 
 const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+    "VarelaRound-Regular": require("./assets/fonts/VarelaRound-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Sunflower-Bold": require("./assets/fonts/Sunflower-Bold.ttf"),
+    "Barlow-Regular": require("./assets/fonts/Barlow-Regular.ttf"),
+    "Urbanist-Regular": require("./assets/fonts/Urbanist-Regular.ttf"),
+    "Urbanist-Bold": require("./assets/fonts/Urbanist-Bold.ttf"),
+    "Urbanist-ExtraBold": require("./assets/fonts/Urbanist-ExtraBold.ttf"),
+    "BarlowCondensed-SemiBold": require("./assets/fonts/BarlowCondensed-SemiBold.ttf"),
+  });
+
   function MaterialIcon({ name, style }) {
     const { height, tintColor, ...iconStyle } = StyleSheet.flatten(style);
     return (
@@ -43,68 +58,49 @@ const App = () => {
     icons: createIconsMap(),
   };
 
-  headerRight = () => (
-    <Image
-      source={require("./assets/iconQrHeader.png")}
-      style={{ height: 45, width: 45 }}
-    ></Image>
-  );
+  if (!fontsLoaded && !error) {
+    return null;
+  }
 
   return (
     <>
       <IconRegistry icons={[MaterialIconsPack]} />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="LoginScreen"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="InicioAlumnoScreen"
-            component={InicioAlumnoScreen}
-            options={{
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: headerRight,
-            }}
-          ></Stack.Screen>
-          <Stack.Screen
-            name="EscaneoScreen"
-            component={EscaneoScreen}
-            options={{
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: headerRight,
-            }}
-          ></Stack.Screen>
-          <Stack.Screen
-            name="NotificarScreen"
-            component={NotificarScreen}
-            options={{
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: headerRight,
-            }}
-          ></Stack.Screen>
-          <Stack.Screen
-            name="PerfilScreen"
-            component={PerfilScreen}
-            options={{
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: headerRight,
-            }}
-          ></Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          {hideSplashScreen ? (
+            <Stack.Navigator
+              initialRouteName="LoginScreen"
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="InicioAlumnoScreen"
+                component={InicioAlumnoScreen}
+                options={{ headerShown: false }}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="EscaneoScreen"
+                component={EscaneoScreen}
+                options={{ headerShown: false }}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="NotificarScreen"
+                component={NotificarScreen}
+                options={{ headerShown: false }}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="PerfilScreen"
+                component={PerfilScreen}
+                options={{ headerShown: false }}
+              ></Stack.Screen>
+            </Stack.Navigator>
+          ) : null}
+        </NavigationContainer>
+      </ApplicationProvider>
     </>
   );
 };
